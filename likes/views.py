1d108e078329f0a266524e1ac06a488e687d7ed9
Undie92 <Undie92@hotmail.com>
2023-04-18 11:07:14 +0000

    added likes and likeview

---
+++ likes/views.py
@@ -0,0 +1,18 @@
+from rest_framework import generics, permissions
+from drf_api.permissions import IsOwnerOrReadOnly
+from likes.models import Likes
+from likes.serializers import LikesSerializer
+
+
+class LikeList(generics.ListCreateAPIView):
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    serializer_class = LikesSerializer
+    queryset = Likes.objects.all()
+    
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+class LikeDetail(generics.RetrieveDestroyAPIView):
+    permission_classes = [IsOwnerOrReadOnly]
+    serializer_class = LikesSerializer
+    queryset = Likes.objects.all()
